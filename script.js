cf918f7ec5af3721b6e777aad47c15a2d50d4048
Kirill <ex@gmail.com>
2022-12-16 00:17:51 +0400

    second stage

--- script.js
+++ script.js
@@ -15,3 +15,23 @@ new Swiper('.swiper', {
   },
 });
 AOS.init();
+
+const footer__input = document.querySelector('.footer-list-input'),
+footer__footerListConfirmbtn = document.querySelector('.footer-list-confirmbtn'),
+open__menu = document.querySelector('.open__menu'),
+header__navMenu = document.querySelector('.header__nav-menu');
+function checker(e){
+  if(this.value){
+    footer__footerListConfirmbtn.classList.add('active');
+  } else{
+    footer__footerListConfirmbtn.classList.remove('active');
+  }
+}
+footer__input.addEventListener('change', checker);
+
+open__menu.addEventListener('click', ()=>{
+  header__navMenu.classList.toggle('active')
+})
+window.addEventListener('scroll', ()=>{
+  header__navMenu.classList.remove('active')
+})
